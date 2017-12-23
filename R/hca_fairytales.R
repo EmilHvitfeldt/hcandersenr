3fe6ac179804cf427bb0eb5356905aed72a155f2
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2017-12-22 17:05:43 -0800

    using rlang::.data to fix scoping problems

--- R/hca_fairytales.R
+++ R/hca_fairytales.R
@@ -12,14 +12,11 @@
 #' @name hca_fairytales
 #'
 #' @examples
-#' \dontrun{
-#' library(tidyverse)
 #' library(hcandersenr)
 #'
 #' hca_fairytales() %>%
 #'   group_by(language) %>%
 #'   summarise(n_lines = n())
-#' }
 #'
 #' @export
 hca_fairytales <- function(){
@@ -27,35 +24,36 @@ hca_fairytales <- function(){
     hcandersenr::hcandersen_da %>%
       dplyr::mutate(language = "Danish") %>%
       dplyr::left_join(
-        hcandersenr::EK %>% dplyr::select("name_da", "name_en"),
+        hcandersenr::EK %>% dplyr::select(.data$name_da, .data$name_en),
         by = c("book" = "name_da")
       ) %>%
-      dplyr::mutate(book = ~name_en) %>%
-      dplyr::select(-~name_en),
+      dplyr::mutate(book = .data$name_en) %>%
+      dplyr::select(-.data$name_en),
     hcandersenr::hcandersen_de %>%
       dplyr::mutate(language = "German") %>%
       dplyr::left_join(
-        hcandersenr::EK %>% dplyr::select("name_de", "name_en"),
+        hcandersenr::EK %>% dplyr::select(.data$name_de, .data$name_en),
         by = c("book" = "name_de")
       ) %>%
-      dplyr::mutate(book = ~name_en) %>%
-      dplyr::select(-~name_en),
-    hcandersenr::hcandersen_en %>% dplyr::mutate(language = "English"),
+      dplyr::mutate(book = .data$name_en) %>%
+      dplyr::select(-.data$name_en),
+    hcandersenr::hcandersen_en %>% 
+      dplyr::mutate(language = "English"),
     hcandersenr::hcandersen_es %>%
       dplyr::mutate(language = "Spanish") %>%
       dplyr::left_join(
-        hcandersenr::EK %>% dplyr::select("name_es", "name_en"),
+        hcandersenr::EK %>% dplyr::select(.data$name_es, .data$name_en),
         by = c("book" = "name_es")
       ) %>%
-      dplyr::mutate(book = ~name_en) %>%
-      dplyr::select(-~name_en),
+      dplyr::mutate(book = .data$name_en) %>%
+      dplyr::select(-.data$name_en),
     hcandersenr::hcandersen_fr %>%
       dplyr::mutate(language = "French") %>%
       dplyr::left_join(
-        hcandersenr::EK %>% dplyr::select("name_fr", "name_en"),
+        hcandersenr::EK %>% dplyr::select(.data$name_fr, .data$name_en),
         by = c("book" = "name_fr")
       ) %>%
-      dplyr::mutate(book = ~name_en) %>%
-      dplyr::select(-~name_en)
+      dplyr::mutate(book = .data$name_en) %>%
+      dplyr::select(-.data$name_en)
   )
 }
